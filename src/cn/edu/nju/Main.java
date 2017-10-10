842ba869a92ca2ad93ee5845c95673f215bacd00
njucjc <njucjc@gmail.com>
2017-10-10 12:22:51 +0800

    add batch

---
+++ src/cn/edu/nju/Main.java
@@ -0,0 +1,27 @@
+package cn.edu.nju;
+
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
+
+import cn.edu.nju.parser.CheckerParser;
+
+public class Main  {
+
+//    @Override
+//    public void start(Stage primaryStage) throws Exception{
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        primaryStage.setTitle("Hello World");
+//        primaryStage.setScene(new Scene(root, 300, 275));
+//        primaryStage.show();
+//    }
+
+
+    public static void main(String[] args) {
+//        launch(args);
+        CheckerParser checkerParser = new CheckerParser("resource/rules.xml","resource/changes/00.txt",2,CheckerParser.PCC_TYPE);
+        checkerParser.run();
+    }
+}
