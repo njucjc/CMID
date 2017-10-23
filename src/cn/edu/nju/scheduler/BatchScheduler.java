5d690ee32f6be52371d70ea776388fcefa15802c
njucjc <njucjc@gmail.com>
2017-10-23 12:51:48 +0800

    rename class

--- src/cn/edu/nju/scheduler/BatchScheduler.java
+++ src/cn/edu/nju/scheduler/BatchScheduler.java
@@ -3,11 +3,11 @@ package cn.edu.nju.scheduler;
 /**
  * Created by njucjc on 2017/10/23.
  */
-public class BatchSchedule implements Scheduler{
+public class BatchScheduler implements Scheduler{
     private int batch; //批处理数
     private int count; //计数
 
-    public BatchSchedule(int batch) {
+    public BatchScheduler(int batch) {
         this.batch = batch;
         this.count = 0;
     }
