8291ff67a859619f5b83870dd09ea90ad67f9b45
njucjc <njucjc@gmail.com>
2017-10-31 18:40:06 +0800

    fix bug: distance

--- src/cn/edu/nju/util/LocationHelper.java
+++ src/cn/edu/nju/util/LocationHelper.java
@@ -4,25 +4,10 @@ package cn.edu.nju.util;
  * Created by njucjc on 2017/10/7.
  */
 public class LocationHelper {
-    private static final double EARTH_RADIUS = 6378.137;
 
-    private static double rad(double d){
-        return d * Math.PI / 180.0;
-    }
 
     public static double getDistance(double long1, double lat1, double long2, double lat2) {
-        double a, b, d, sa2, sb2;
-        lat1 = rad(lat1);
-        lat2 = rad(lat2);
-        a = lat1 - lat2;
-        b = rad(long1 - long2);
-
-        sa2 = Math.sin(a / 2.0);
-        sb2 = Math.sin(b / 2.0);
-        d = 2   * EARTH_RADIUS
-                * Math.asin(Math.sqrt(sa2 * sa2 + Math.cos(lat1)
-                * Math.cos(lat2) * sb2 * sb2));
-        return d;
+       return Math.sqrt((long1 - long2) * (long1 - long2) + (lat1 - lat2) * (lat1 - lat2));
     }
 
     public static void main(String[] args) {
