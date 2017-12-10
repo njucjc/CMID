cb1546d1c4c0af65cd36c0fb55a0e29c50a41a50
njucjc <njucjc@gmail.com>
2017-12-10 21:04:45 +0800

    add framework

--- src/cn/edu/nju/server/Server.java
+++ src/cn/edu/nju/server/Server.java
@@ -1,6 +1,6 @@
 package cn.edu.nju.server;
 
-import cn.edu.nju.client.Client;
+import cn.edu.nju.builder.AbstractCheckerBuilder;
 import cn.edu.nju.context.Context;
 import cn.edu.nju.context.ContextParser;
 import cn.edu.nju.util.TimestampHelper;
@@ -10,22 +10,31 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
+import java.util.*;
 
 /**
  * Created by njucjc on 2017/10/29.
  */
 
 
-public class Server extends Thread{
+public class Server extends AbstractCheckerBuilder implements Runnable{
 
     private DataInputStream inputFromClient;
 
     private DataOutputStream outputToClient;
 
+    private boolean finished = false;
+
+    private Queue<Context> contextQueue = new LinkedList<>();
+
+    private Timer deleteTimer = new Timer();
+
+    private Timer checkTimer = new Timer();
+
     ContextParser contextParser = new ContextParser();
 
-    public Server()  {
+    public Server(String configFilePath)  {
+        super(configFilePath);
         try {
             ServerSocket serverSocket = new ServerSocket(8000);
             System.out.println("Server started at " + new Date());
@@ -39,6 +48,25 @@ public class Server extends Thread{
         }catch(IOException e) {
             e.printStackTrace();
         }
+
+        checkTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                synchronized (this){
+                    System.out.println("1:" + new Date());
+                }
+            }
+        }, 10,10);
+
+
+        deleteTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                synchronized (this) {
+                    System.out.println("2:" + new Date());
+                }
+            }
+        },1000, 1000);
     }
 
     @Override
@@ -50,11 +78,14 @@ public class Server extends Thread{
                 count++;
                 if ("exit".equals(pattern)) {
                     System.out.println("Good bye! Server closed at " + new Date());
+                    finished = true;
                     break;
                 }
                 Context context = contextParser.parseContext(count,pattern);
                 context.setTimestamp(TimestampHelper.getCurrentTimestamp());
-                System.out.println(context.getTimestamp());
+                synchronized (this) {
+                    contextQueue.offer(context);
+                }
 
                 System.out.println("[DEBUG] '+' " + pattern);
             }
@@ -62,11 +93,16 @@ public class Server extends Thread{
         catch (IOException e) {
             e.printStackTrace();
         }
+
+        if(finished) {
+            checkTimer.cancel();
+            deleteTimer.cancel();
+        }
     }
 
 
 
     public static void main(String[] args) {
-        new Server().start();
+        new Thread(new Server(args[0])).start();
     }
 }
