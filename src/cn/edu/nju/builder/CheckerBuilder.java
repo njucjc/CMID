85a7c4832b138ebd91e1fe7a7b23f328dffca406
njucjc <njucjc@gmail.com>
2018-03-08 00:58:51 +0800

    add run time

--- src/cn/edu/nju/builder/CheckerBuilder.java
+++ src/cn/edu/nju/builder/CheckerBuilder.java
@@ -42,14 +42,17 @@ public class CheckerBuilder  extends AbstractCheckerBuilder implements Runnable{
         long endTime = System.nanoTime(); //获取结束时间
         int incCount = 0;
         int checkTimes = 0;
+        long timeCount = 0L;
         for(Checker checker : checkerList) {
             incCount += checker.getInc();
             checkTimes += checker.getCheckTimes();
+            timeCount = timeCount + checker.getTimeCount();
             LogFileHelper.getLogger().info(checker.getName() + ": " + checker.getInc() + " times" );
         }
         LogFileHelper.getLogger().info("Total INC: " + incCount + " times");
         LogFileHelper.getLogger().info("Total check: " + checkTimes + " times");
-        LogFileHelper.getLogger().info("run time： " + (endTime - startTime) / 1000000 + " ms");
+        LogFileHelper.getLogger().info("check time: " + timeCount / 1000000 + " ms");
+        LogFileHelper.getLogger().info("run time: " + (endTime - startTime) / 1000000 + " ms");
         shutdown();
     }
 
--- src/cn/edu/nju/server/Server.java
+++ src/cn/edu/nju/server/Server.java
@@ -42,6 +42,7 @@ public class Server extends AbstractCheckerBuilder implements Runnable{
         running = true;
 
         long count = 0;
+        long startTime = System.nanoTime();
         try {
             while (running) {
                 DatagramPacket packet = new DatagramPacket(buf, buf.length);
@@ -76,6 +77,7 @@ public class Server extends AbstractCheckerBuilder implements Runnable{
         LogFileHelper.getLogger().info("Total Inc: " + inc);
         LogFileHelper.getLogger().info("Receive: " + count );
         LogFileHelper.getLogger().info("check time: " + time / 1000000 + " ms");
+        LogFileHelper.getLogger().info("run time: " + (endTime - startTime) / 1000000 + " ms");
         shutdown();
     }
 
