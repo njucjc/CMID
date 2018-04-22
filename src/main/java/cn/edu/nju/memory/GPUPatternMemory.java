034c0f5f1070bac869f357b35777acf620f669da
njucjc <njucjc@gmail.com>
2018-04-22 15:19:41 +0800

    add GPUPatternMemory

--- src/main/java/cn/edu/nju/memory/GPUPatternMemory.java
+++ src/main/java/cn/edu/nju/memory/GPUPatternMemory.java
@@ -1,4 +1,47 @@
 package cn.edu.nju.memory;
 
+import static jcuda.driver.JCudaDriver.*;
+
+import jcuda.Pointer;
+import jcuda.Sizeof;
+import jcuda.driver.CUdeviceptr;
+
+import java.util.*;
+
 public class GPUPatternMemory {
+
+    Map<String, CUdeviceptr> beginMap = new HashMap<>();
+
+    Map<String, CUdeviceptr> lengthMap = new HashMap<>();
+
+    Map<String, CUdeviceptr> contextsMap = new HashMap<>();
+
+    public GPUPatternMemory(String [] keySet) {
+        for(String key : keySet) {
+            CUdeviceptr begin = new CUdeviceptr();
+            CUdeviceptr length = new CUdeviceptr();
+            CUdeviceptr contexts = new CUdeviceptr();
+
+            cuMemAlloc(begin, Sizeof.INT);
+            cuMemcpyHtoD(begin, Pointer.to(new int[]{0}), Sizeof.INT);
+
+            cuMemAlloc(length, Sizeof.INT);
+            cuMemcpyHtoD(length, Pointer.to(new int[]{0}), Sizeof.INT);
+
+            cuMemAlloc(contexts, Config.MAX_PATTERN_SIZE * Sizeof.INT);
+
+            beginMap.put(key, begin);
+            lengthMap.put(key, length);
+            contextsMap.put(key, contexts);
+        }
+    }
+
+    public void free() {
+        for(String key : beginMap.keySet()) {
+            cuMemFree(beginMap.get(key));
+            cuMemFree(lengthMap.get(key));
+            cuMemFree(contextsMap.get(key));
+        }
+    }
+
 }
