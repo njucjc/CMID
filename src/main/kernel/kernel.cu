ee0901b9649b4f83a6b9eed48e1d95fcd3a2564a
njucjc <njucjc@gmail.com>
2018-04-24 01:13:32 +0800

    kernel function first version

--- src/main/kernel/kernel.cu
+++ src/main/kernel/kernel.cu
@@ -19,54 +19,82 @@ enum Type {
 };
 
 #define MAX_PARAM_NUM 2
+#define MAX_PATTERN_SIZE 500
+#define MAX_LINK_SIZE 40
 
-__device__ double distance(double latitude1, double latitude2, double longitude1, double longitude2)
-{
-	return ((latitude1 - latitude2) * (latitude1 - latitude2) + (longitude1 - longitude2) * (longitude1 - longitude2));
+struct Context{
+	int id;
+	double latitude;
+	double longitude;
+	double speed;
+};
+
+struct Links {
+	int length;
+	int link_pool[MAX_LINK_SIZE][MAX_PARAM_NUM];
+};
+
+
+__device__ bool same(Context c1, Context c2){
+	return (c1.id == c2.id);
 }
 
-
-__device__ bool same(int id1, int id2)
-{
-	return id1 == id2;
+__device__ bool sz_spd_close(Context c1, Context c2){
+	return ((c1.speed - c2.speed) >= -50.0 && (c1.speed - c2.speed) <= 50.0);
 }
 
-__device__ bool sz_spd_close(double speed1, double speed2) 
-{
-	return ((speed1 - speed2) >= -50.0 && (speed1 - speed2) <= 50.0);
+__device__ bool sz_loc_close(Context c1, Context c2){
+	return ((c1.latitude - c2.latitude) * (c1.latitude - c2.latitude) + (c1.longitude - c2.longitude) * (c1.longitude - c2.longitude)) <= 0.000001;
 }
 
-__device__ bool sz_loc_close(double latitude1, double latitude2, double longitude1, double longitude2) 
-{
-	return distance(latitude1, latitude2, longitude1, longitude2) <= 0.000001;
+__device__ bool sz_loc_dist(Context c1, Context c2){
+	return ((c1.latitude - c2.latitude) * (c1.latitude - c2.latitude) + (c1.longitude - c2.longitude) * (c1.longitude - c2.longitude)) <= 0.000625;
 }
 
-__device__ bool sz_loc_dist(double latitude1, double latitude2, double longitude1, double longitude2) 
-{
-	return  distance(latitude1, latitude2, longitude1, longitude2) <= 0.000625;
-}
-
-__device__ bool sz_loc_dist_neq(double latitude1, double latitude2, double longitude1, double longitude2) 
-{
-	double dist = distance(latitude1, latitude2, longitude1, longitude2);
+__device__ bool sz_loc_dist_neq(Context c1, Context c2){
+	double dist = ((c1.latitude - c2.latitude) * (c1.latitude - c2.latitude) + (c1.longitude - c2.longitude) * (c1.longitude - c2.longitude));
 	bool result = true;
-	if (dist > 0.000625 || dist == 0) {
-		result = false;
-	}
-	return result;
+    if (dist > 0.000625 || dist == 0) {
+    	result = false;
+    }
+    return result;
 	//return (dist <= 0.000625) && (dist != 0);
 }
 
-__device__ bool sz_loc_range(double latitude, double longitude)
-{
-	return longitude >= 112.0 && longitude <= 116.0 && latitude >= 20.0 && latitude <= 24.0;
+__device__ bool sz_loc_range(Context c){
+	return c.longitude >= 112.0 && c.longitude <= 116.0 && c.latitude >=20.0 && c.latitude <= 24.0;
 }
 
-__device__ int calc_offset(	int node, int tid, int *branch_idx,
+__device__ void linkHelper(Links *left, Links *right) {
+	int left_len = left->length;
+	int right_len = right->length;
+
+
+	for (int i = 0; i < right_len; i++) {
+		int j;
+		for (j = 0; j < left_len; j++) {
+			if (right->link_pool[i][0] == left->link_pool[j][0] && right->link_pool[i][1] == left->link_pool[j][1]) {
+				break;
+			}
+		}
+		if (j == left_len) {
+			left->link_pool[left->length][0] = right->link_pool[i][0];
+			left->link_pool[left->length][1] = right->link_pool[i][1];
+			left->length = (left->length + 1) % MAX_LINK_SIZE;
+		}
+
+	}
+}
+
+__device__ int calc_offset(	int node, int tid, Context *params,
 							int *parent, int *left_child, int *right_child, int *node_type, int *pattern_idx,
-							int *pattern_length,
-							int *branch_size)
-{
+							int *pattern_begin, int *pattern_length, int *pattern,
+							double *longitude, double *latitude, double *speed, // contexts
+							int *branch_size) {
+	if (parent[node] == -1) {
+		return branch_size[node] - 1;
+	}
+
 	int offset = 0;
 	int current_node = node;
 	int index = 0, tmp = tid;
@@ -74,9 +102,15 @@ __device__ int calc_offset(	int node, int tid, int *branch_idx,
 		int type = node_type[parent[current_node]];
 		if (type == Type::EXISTENTIAL_NODE || type == Type::UNIVERSAL_NODE) {
 			int len = pattern_length[pattern_idx[parent[current_node]]];
-			branch_idx[index] = tmp % len;
+			int branch_idx = tmp % len;
 			tmp /= len;
-			offset += (branch_idx[index] + 1) * branch_size[current_node];
+
+			params[index].id = (pattern + pattern_idx[parent[current_node]] * MAX_PATTERN_SIZE)[(branch_idx + pattern_begin[parent[current_node]]) % MAX_PATTERN_SIZE];
+			params[index].latitude = latitude[params[index].id];
+			params[index].longitude = longitude[params[index].id];
+			params[index].speed = speed[params[index].id];
+			
+			offset += (branch_idx + 1) * branch_size[current_node];
 			index++;
 		}
 		else if (type == Type::AND_NODE || type == Type::IMPLIES_NODE || type == Type::OR_NODE) {
@@ -98,31 +132,176 @@ __device__ int calc_offset(	int node, int tid, int *branch_idx,
 
 
 extern "C"
-__global__ void gen_truth_value(int *parent, int *left_child, int *right_hild, int *node_type, int *pattern_idx, //constraint rule 
+__global__ void gen_truth_value(int *parent, int *left_child, int *right_child, int *node_type, int *pattern_idx, //constraint rule 
 								int *branch_size, int cunit_begin, int cunit_end,//cunit_end is the root of cunit
-								int pattern_num, int *pattern_begin, int *pattern_length, int *pattern, //patterns
+								int *pattern_begin, int *pattern_length, int *pattern, //patterns
 								double *longitude, double *latitude, double *speed, // contexts
-								int *truth_values)
-{
+								int *truth_values) {
 	int tid = threadIdx.x + blockDim.x * blockIdx.x;
-	int branch_idx[MAX_PARAM_NUM];
-	int ccopy_root_offset = calc_offset(cunit_end, tid, branch_idx, parent, left_child, right_hild, node_type, pattern_idx, pattern_length, branch_size);
+	Context params[MAX_PARAM_NUM];
+	int ccopy_root_offset = calc_offset(cunit_end, tid, params, 
+										parent, left_child, right_child, node_type, pattern_idx, 
+										pattern_begin, pattern_length, pattern,
+										longitude, latitude, speed,
+										branch_size);
 
 	for (int node = cunit_begin; node <= cunit_end; node++) {
 		int offset = ccopy_root_offset - (cunit_end - node);
-
+		int type = node_type[node];
+		bool value;
+		if(type == Type::UNIVERSAL_NODE) {
+			int step = branch_size[left_child[node]];
+			value = true;
+			for (int i = 0; i < pattern_length[pattern_idx[node]]; i++) {
+				value = value && truth_values[offset - (i * step + 1)];
+			}
+		}
+		else if (type == Type::EXISTENTIAL_NODE) {
+			int step = branch_size[left_child[node]];
+			value = false;
+			for (int i = 0; i < pattern_length[pattern_idx[node]]; i++) {
+				value = value || truth_values[offset - (i * step + 1)];
+			}
+		}
+		else if (type == Type::AND_NODE) {
+			//right && left
+			value = truth_values[offset - 1] && truth_values[offset - (branch_size[right_child[node]] + 1)];
+		}
+		else if (type == Type::OR_NODE) {
+			//right || left
+			value = truth_values[offset - 1] || truth_values[offset - (branch_size[right_child[node]] + 1)];
+		}
+		else if (type == Type::IMPLIES_NODE) {
+			//!left || right
+			value = !truth_values[offset - (branch_size[right_child[node]] + 1)] || truth_values[offset - 1];
+		}
+		else if (type == Type::NOT_NODE) {
+			value = !truth_values[offset - 1];
+		}
+		else if (type == Type::SAME) {
+			value = same(params[0], params[1]);
+		}
+		else if (type == Type::SZ_SPD_CLOSE) {
+			value = sz_spd_close(params[0], params[1]);
+		}
+		else if (type == Type::SZ_LOC_CLOSE) {
+			value = sz_loc_close(params[0], params[1]);
+		}
+		else if (type == Type::SZ_LOC_DIST) {
+			value = sz_loc_dist(params[0], params[1]);
+		}
+		else if (type == Type::SZ_LOC_DIST_NEQ) {
+			value = sz_loc_dist_neq(params[0], params[1]);
+		}
+		else if (type == Type::SZ_LOC_RANGE) {
+			value = sz_loc_range(params[0]);
+		}
+		truth_values[offset] = value;
 	}
 
  }
 
  extern "C"
- __global__ void gen_links()
- {
+ __global__ void gen_links(int *parent, int *left_child, int *right_child, int *node_type, int *pattern_idx, //constraint rule 
+	 int *branch_size, int cunit_begin, int cunit_end,//cunit_end is the root of cunit
+	 int *pattern_begin, int *pattern_length, int *pattern, //patterns
+	 double *longitude, double *latitude, double *speed, // contexts
+	 int *truth_values, 
+	 Links *links, int *link_result, int *link_num, 
+	 int last_cunit_root) {
+
+	 int tid = threadIdx.x + blockDim.x * blockIdx.x;
+	 Context params[MAX_PARAM_NUM];
+
+	 for (int i = 0; i < MAX_PARAM_NUM; i++) {
+		 params[i].id = -1;
+	 }
+
+	 int ccopy_root_offset = calc_offset(cunit_end, tid, params,
+		 parent, left_child, right_child, node_type, pattern_idx,
+		 pattern_begin, pattern_length, pattern,
+		 longitude, latitude, speed,
+		 branch_size);
+
+	 for (int node = cunit_begin; node <= cunit_end; node++) {
+		 int offset = ccopy_root_offset - (cunit_end - node);
+		 int type = node_type[node];
+		 bool value = truth_values[offset];
+		 Links* cur_links = &links[offset];
+		 cur_links->length = 0;
+		 if (type == Type::UNIVERSAL_NODE || type == Type::EXISTENTIAL_NODE) {
+			 int step = branch_size[left_child[node]];
+			 for (int i = 0; i < pattern_length[pattern_idx[node]]; i++) {
+				 if (truth_values[offset - (i * step + 1)] == value) {
+					 linkHelper(cur_links, &links[offset - (i * step + 1)]);
+				 }
+			 }
+		 }
+		 else if (type == Type::AND_NODE || type == Type::OR_NODE) {
+			 if (truth_values[offset - 1] == value) {
+				 linkHelper(cur_links, &links[offset - 1]);
+			 }
+			 
+			 if (truth_values[offset - (branch_size[right_child[node]] + 1)] == value) {
+				 linkHelper(cur_links, &links[offset - (branch_size[right_child[node]] + 1)]);
+			 }
+		 }
+		 else if (type == Type::IMPLIES_NODE) {
+			 //!left || right
+			 bool left = truth_values[offset - (branch_size[right_child[node]] + 1)];
+			 bool right = truth_values[offset - 1];
+
+			 if ((!left && right) || left && !right) {
+				 linkHelper(cur_links, &links[offset - 1]);
+				 linkHelper(cur_links, &links[offset - (branch_size[right_child[node]] + 1)]);
+			 }
+			 else if(left && right){
+				 linkHelper(cur_links, &links[offset - 1]);
+			 }
+			 else if (left && right) {
+				 linkHelper(cur_links, &links[offset - (branch_size[right_child[node]] + 1)]);
+			 }
+		 }
+		 else if (type == Type::NOT_NODE) {
+			 linkHelper(cur_links, &links[offset - 1]);
+		 }
+		 else if (type == Type::SAME
+			 || type == Type::SZ_SPD_CLOSE
+			 || type == Type::SZ_LOC_CLOSE
+			 || type == Type::SZ_LOC_DIST
+			 || type == Type::SZ_LOC_DIST_NEQ
+			 || type == Type::SZ_LOC_RANGE) {
+			 cur_links->length = 1;
+			 for (int i = 0; i < MAX_PARAM_NUM; i++) {
+				 cur_links->link_pool[0][i] = params[i].id;
+			 }
+		 }
+
+		 if (last_cunit_root == cunit_end) {
+			 *link_num = links[branch_size[cunit_end]].length;
+
+			 for (int i = 0; i < *link_num; i++) {
+				 link_result[i] = links[branch_size[cunit_end]].link_pool[i][0];
+				 link_result[i + 1] = links[branch_size[cunit_end]].link_pool[i][1];
+			 }
+		 }
+		 
+	 }
 
   }
 
 extern "C"
-__global__ void update_pattern()
-{
+__global__ void update_pattern(int op, int pattern_idx,
+							   int *pattern_begin, int *pattern_length, int *pattern,
+							   int id) {
+	if (op == 0) { //-
+		pattern_begin[pattern_idx] = (pattern_begin[pattern_idx] + 1) % MAX_PATTERN_SIZE;
+		pattern_length[pattern_idx]--;
+	}
+	else if (op == 1) {//+
+		(pattern + pattern_idx * MAX_PATTERN_SIZE)[(pattern_begin[pattern_idx] + pattern_length[pattern_idx]) % MAX_PATTERN_SIZE];
+		pattern_length[pattern_idx]++;
+
+	}
 
 }
