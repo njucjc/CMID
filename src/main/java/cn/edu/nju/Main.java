e343f0e0db14669f70ba0da3e25f2fbcf5a79317
njucjc <njucjc@gmail.com>
2018-04-24 17:10:09 +0800

    modify main

--- src/main/java/cn/edu/nju/Main.java
+++ src/main/java/cn/edu/nju/Main.java
@@ -9,9 +9,11 @@ public class Main  {
 
     public static void main(String[] args) {
         if (args.length == 1) {
-            Thread checkerThread = new Thread(new CheckerBuilder(args[0]));
-            checkerThread.setPriority(Thread.MAX_PRIORITY);
-            checkerThread.start();
+//            Thread checkerThread = new Thread(new CheckerBuilder(args[0]));
+//            checkerThread.setPriority(Thread.MAX_PRIORITY);
+//            checkerThread.start();
+            CheckerBuilder builder = new CheckerBuilder(args[0]);
+            builder.run();
         }
         else {
             System.out.println("Usage: java Main [configFilePath].");
