4fb7901415f19d29d9ae36196d741757edf7257c
njucjc <njucjc@gmail.com>
2018-09-03 19:21:21 +0800

    modify switcher

--- src/main/java/cn/edu/nju/switcher/SimpleSwitcher.java
+++ src/main/java/cn/edu/nju/switcher/SimpleSwitcher.java
@@ -8,39 +8,53 @@ import cn.edu.nju.checker.CheckerType;
  */
 public class SimpleSwitcher implements Switcher {
 
+    private final static int THRESHOLD = 400;
+
+    private final static int STEP = 5;
+
     private int checkerType;
 
     private int schedulerType;
 
- //   private int count;
+    private int count;
+
+    private int totalDelay;
 
     public SimpleSwitcher(int checkerType, int schedulerType) {
         this.checkerType = checkerType;
         this.schedulerType = schedulerType;
-    //    this.count = 0;
+        this.count = 0;
+        this.totalDelay = 0;
     }
 
     @Override
     public synchronized boolean isSwitch(long delay) {
         boolean needSwitch = false;
 
-        switch (checkerType) {
-            case CheckerType.ECC_TYPE: {
-                if(delay> 300) {
-                    needSwitch = true;
-                    checkerType = CheckerType.CON_TYPE;
-                }
-                break;
-            }
+        count = (count + 1) % STEP;
+        totalDelay += delay;
 
-            case CheckerType.CON_TYPE: {
-                if(delay > 300) {
-                    needSwitch = true;
-                    checkerType = CheckerType.PCC_TYPE;
-                }
-                break;
-            }
+        if(count == 0) {
 
+            switch (checkerType) {
+                case CheckerType.ECC_TYPE: {
+                    if (totalDelay / STEP > THRESHOLD) {
+                        needSwitch = true;
+                        checkerType = CheckerType.CON_TYPE;
+                    }
+                    break;
+                }
+
+                case CheckerType.CON_TYPE: {
+                    if (totalDelay / STEP > THRESHOLD) {
+                        needSwitch = true;
+                        checkerType = CheckerType.PCC_TYPE;
+                    }
+                    break;
+                }
+
+            }
+            totalDelay = 0;
         }
         return needSwitch;
     }
