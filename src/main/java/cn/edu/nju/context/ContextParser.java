86183fb297458fce16d10d1af0ce6b72672a5ee9
njucjc <njucjc@gmail.com>
2018-09-07 12:28:05 +0800

    data field update

--- src/main/java/cn/edu/nju/context/ContextParser.java
+++ src/main/java/cn/edu/nju/context/ContextParser.java
@@ -7,11 +7,11 @@ public class ContextParser {
     public synchronized Context parseContext(int id, String pattern) {
         String [] fields = pattern.split(",");
         String timestamp = fields[0];
-        String plateNumber = fields[2];
-        double longitude = Double.parseDouble(fields[3]);
-        double latitude = Double.parseDouble(fields[4]);
-        double speed = Double.parseDouble(fields[5]);
-        int status = Integer.parseInt(fields[7]);
+        String plateNumber = fields[1];
+        double longitude = Double.parseDouble(fields[2]);
+        double latitude = Double.parseDouble(fields[3]);
+        double speed = Double.parseDouble(fields[4]);
+        int status = Integer.parseInt(fields[6]);
 
         return new Context(id, timestamp, plateNumber, longitude, latitude, speed, status);
     }
