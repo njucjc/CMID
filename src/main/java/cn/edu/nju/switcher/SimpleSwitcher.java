420a148d7df1eeda2444a45ba5e74d9b3e2d37fd
njucjc <njucjc@gmail.com>
2018-09-13 12:45:15 +0800

    add param config

--- src/main/java/cn/edu/nju/switcher/SimpleSwitcher.java
+++ src/main/java/cn/edu/nju/switcher/SimpleSwitcher.java
@@ -3,14 +3,21 @@ package cn.edu.nju.switcher;
 
 import cn.edu.nju.checker.CheckerType;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Created by njucjc at 2018/7/24
  */
 public class SimpleSwitcher implements Switcher {
 
-    private final static int THRESHOLD = 50;
+//    private final static int THRESHOLD = 70;
+//
+//    private final static int STEP = 5;
+    private int maxDelay;
 
-    private final static int STEP = 5;
+    private  int step;
 
     private int checkerType;
 
@@ -20,11 +27,27 @@ public class SimpleSwitcher implements Switcher {
 
     private int totalDelay;
 
-    public SimpleSwitcher(int checkerType, int schedulerType) {
+    public SimpleSwitcher(String paramFilePath, int checkerType, int schedulerType) {
         this.checkerType = checkerType;
         this.schedulerType = schedulerType;
         this.count = 0;
         this.totalDelay = 0;
+
+        parseParamFile(paramFilePath);
+    }
+
+    private void parseParamFile(String paramFilePath) {
+        Properties properties = new Properties();
+        try {
+            FileInputStream fis = new FileInputStream(paramFilePath);
+            properties.load(fis);
+            fis.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.maxDelay = Integer.parseInt(properties.getProperty("maxDelay"));
+        this.step = Integer.parseInt(properties.getProperty("step"));
     }
 
     @Override
@@ -32,7 +55,7 @@ public class SimpleSwitcher implements Switcher {
         boolean needSwitch = false;
 
         if(delay != 0) {
-            count = (count + 1) % STEP;
+            count = (count + 1) % step;
             totalDelay += delay;
         }
 
@@ -40,7 +63,7 @@ public class SimpleSwitcher implements Switcher {
 
             switch (checkerType) {
                 case CheckerType.ECC_TYPE: {
-                    if (totalDelay / STEP > THRESHOLD) {
+                    if (totalDelay / step > maxDelay) {
                         needSwitch = true;
                         checkerType = CheckerType.CON_TYPE;
                     }
@@ -48,7 +71,7 @@ public class SimpleSwitcher implements Switcher {
                 }
 
                 case CheckerType.CON_TYPE: {
-                    if (totalDelay / STEP > THRESHOLD) {
+                    if (totalDelay / step > maxDelay) {
                         needSwitch = true;
                         checkerType = CheckerType.PCC_TYPE;
                     }
