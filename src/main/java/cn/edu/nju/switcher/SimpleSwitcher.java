f60c6dfd511010cbdc63c95923a1b84fde66e528
njucjc <njucjc@gmail.com>
2019-10-20 12:34:53 +0800

    refactor

--- src/main/java/cn/edu/nju/switcher/SimpleSwitcher.java
+++ src/main/java/cn/edu/nju/switcher/SimpleSwitcher.java
@@ -2,12 +2,10 @@ package cn.edu.nju.switcher;
 
 
 import cn.edu.nju.checker.CheckerType;
-import cn.edu.nju.util.LogFileHelper;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
-import java.lang.management.OperatingSystemMXBean;
 import java.lang.management.RuntimeMXBean;
 import java.lang.management.ThreadMXBean;
 import java.util.Properties;
@@ -17,44 +15,70 @@ import java.util.Properties;
  */
 public class SimpleSwitcher implements Switcher {
 
+    private ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+
+    private RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+
+    private long initUpTime;
+
+    private long initCPUTime;
+
     private int checkerType;
 
     private int schedulerType;
 
     private int count;
 
+    private int step;
 
-    public SimpleSwitcher(int checkerType, int schedulerType) {
+    private float cpuUsageLow;
+
+
+    public SimpleSwitcher(String paramFile, int checkerType, int schedulerType) {
+        this.initUpTime = runtimeMXBean.getUptime();
+        this.initCPUTime = threadMXBean.getCurrentThreadCpuTime();
         this.checkerType = checkerType;
         this.schedulerType = schedulerType;
-        this.count = -1;
+        this.count = 0;
+        parseParamFile(paramFile);
     }
 
 
     @Override
     public boolean isSwitch(int num) {
         boolean needSwitch = false;
-        if (count + 1 != num) {
-            needSwitch = true;
-        }
+
         count++;
 
-        if (needSwitch) {
-
-            count = num;
+        if (count % step == 0) {
             switch (checkerType) {
                 case CheckerType.ECC_TYPE: {
-                    checkerType = CheckerType.CON_TYPE;
+                    if (count != num) {
+                        checkerType = CheckerType.CON_TYPE;
+                        schedulerType = 1; //ImmedSched
+                        needSwitch = true;
+                        count = num + 1;
+                    }
                     break;
                 }
 
                 case CheckerType.CON_TYPE: {
-                    checkerType = CheckerType.PCC_TYPE;
+                    if (count != num) {
+                        checkerType = CheckerType.PCC_TYPE;
+                        schedulerType = 1;
+                        needSwitch = true;
+                        count = num + 1;
+                    }
                     break;
                 }
 
                 case CheckerType.PCC_TYPE: {
-                    checkerType = CheckerType.CONPCC_TYPE;
+                    if (count != num) {
+                        checkerType = CheckerType.CONPCC_TYPE;
+                        schedulerType = 1;
+                        needSwitch = true;
+                        count = num + 1;
+                    }
                     break;
                 }
 
@@ -76,5 +100,30 @@ public class SimpleSwitcher implements Switcher {
     @Override
     public int getSchedulerType() {
         return schedulerType;
+    }
+
+
+    private float getCPURate() {
+        int nrCPUs = ManagementFactory.getOperatingSystemMXBean().getAvailableProcessors();
+        long elapsedCPUTime = threadMXBean.getCurrentThreadCpuTime() - initCPUTime;
+        long elapsedUpTime = runtimeMXBean.getUptime() - initUpTime;
+
+        float cpuRate = elapsedCPUTime * 100 / (elapsedUpTime * 1000000F * nrCPUs);
+
+        return cpuRate;
+    }
+
+    private void parseParamFile(String paramFilePath) {
+        Properties properties = new Properties();
+        try {
+            FileInputStream fis = new FileInputStream(paramFilePath);
+            properties.load(fis);
+            fis.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.step = Integer.parseInt(properties.getProperty("step"));
+        this.cpuUsageLow = Float.parseFloat(properties.getProperty("cpuUsageLow"));
     }
 }
