77f52c8f89f6bc490fa672536c4217b5de1329a3
njucjc <njucjc@gmail.com>
2020-11-17 10:59:54 +0800

    add some handler

--- src/main/java/cn/edu/nju/view/MainFrameController.java
+++ src/main/java/cn/edu/nju/view/MainFrameController.java
@@ -3,10 +3,7 @@ import cn.edu.nju.MainApp;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.*;
 
 public class MainFrameController {
 
@@ -69,19 +66,25 @@ public class MainFrameController {
     private Button start;
 
     @FXML
-    private Button end;
+    private Button stop;
 
     @FXML
     private Button exit;
+
+    @FXML
+    private SplitPane splitPane;
 
     @FXML
     private void initialize() {
         techSelect.getItems().addAll("ECC", "Con-C", "GAIN", "PCC", "CPCC");
         schedSelect.getItems().addAll("Immed", "GEAS-ori", "GEAS-opt");
         runTypeSelect.getItems().addAll("static-change-based", "dynamic-change-based", "static-time-based", "dynamic-time-based");
-        concurrentSelect.getItems().addAll(4, 8, 16);
+        concurrentSelect.getItems().addAll(2, 4, 8, 16);
 
+        oracleFileSelect.setDisable(true);
+        runTypeSelect.setValue("static-change-based");
         runTypeSelect.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            runTypeSelect.setValue(newValue);
             if (newValue.contains("time")) {
                 schedSelect.getItems().remove(0, schedSelect.getItems().size());
                 schedSelect.getItems().addAll("Immed");
@@ -90,10 +93,19 @@ public class MainFrameController {
                 schedSelect.getItems().remove(0, schedSelect.getItems().size());
                 schedSelect.getItems().addAll("Immed", "GEAS-ori", "GEAS-opt");
             }
+
+            if (newValue.contains("static")) {
+                oracleFileSelect.setDisable(true);
+            }
+            else {
+                oracleFileSelect.setDisable(false);
+            }
         });
 
         concurrentSelect.setDisable(true);
+        techSelect.setValue("ECC");
         techSelect.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            techSelect.setValue(newValue);
             if (newValue.equals("Con-C")) {
                 concurrentSelect.setDisable(false);
             }
@@ -101,6 +113,15 @@ public class MainFrameController {
                 concurrentSelect.setDisable(true);
             }
         });
+
+        schedSelect.setValue("Immed");
+        schedSelect.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> { schedSelect.setValue(newValue); });
+
+        concurrentSelect.setValue(2);
+        concurrentSelect.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> { concurrentSelect.setValue(newValue); });
+
+        double p = splitPane.getDividers().get(0).getPosition();
+        splitPane.getDividers().get(0).positionProperty().addListener((observable, oldValue, newValue) -> splitPane.getDividers().get(0).setPosition(p));
 
 
         // 数据展示区初始化
@@ -123,6 +144,43 @@ public class MainFrameController {
         //  missIncSeries.setName("漏报:10");
         //  missIncSeries.getData().set(0, new XYChart.Data<>("  ",10));
         //  incChart.getData().addAll(showIncSeries, faultIncSeries, missIncSeries);
+    }
+
+    private void setDisableSelect(boolean disable) {
+        ruleFileSelect.setDisable(disable);
+        patternFileSelect.setDisable(disable);
+        techSelect.setDisable(disable);
+        schedSelect.setDisable(disable);
+        runTypeSelect.setDisable(disable);
+        dataFileSelect.setDisable(disable);
+        if (!disable) {
+            oracleFileSelect.setDisable(!runTypeSelect.getValue().contains("dynamic"));
+            concurrentSelect.setDisable(!techSelect.getValue().equals("Con-C"));
+        }
+        else {
+            oracleFileSelect.setDisable(true);
+            oracleFileSelect.setDisable(true);
+        }
+    }
+
+    /**
+     * 开始系统
+     */
+    @FXML
+    private void handleStartSystem() {
+        start.setDisable(true);
+        setDisableSelect(true);
+        //TODO：stopped = false
+    }
+
+    /**
+     * 停止系统
+     */
+    @FXML
+    private void handleStopSystem() {
+        start.setDisable(false);
+        setDisableSelect(false);
+        //TODO: stopped = true
     }
 
     /**
