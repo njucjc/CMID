5b18dcd33a8fecb30f857d94f3db92fa585b0980
njucjc <njucjc@gmail.com>
2020-12-14 14:11:22 +0800

    static run

--- src/main/java/cn/edu/nju/builder/CheckerBuilder.java
+++ src/main/java/cn/edu/nju/builder/CheckerBuilder.java
@@ -31,6 +31,14 @@ public class CheckerBuilder  extends AbstractCheckerBuilder{
         System.out.println("[INFO] 开始一致性检测......");
 
 
+        String timestamp1 = null;
+        String timestamp2 = null;
+
+        long sum1 = 0;
+        long sum2 = 0;
+
+        long count1 = 0;
+        long count2 = 0;
         while (dataCount < contextList.size()) {
             System.out.print("[INFO] 当前进度: " + (dataCount + 1) + "/" + contextList.size() + '\r');
 
@@ -45,9 +53,35 @@ public class CheckerBuilder  extends AbstractCheckerBuilder{
                 changeHandler.doContextChange(dataCount, change);
                 if (dataCount >= 1) {
                     interval = diff(contextList.get(dataCount), contextList.get(dataCount - 1));
+
+                    sum1 += interval;
+                    sum2 += interval;
+                }
+                else { // 初始化
+                    timestamp1 = getTimestamp(contextList.get(0));
+                    timestamp2 = timestamp1;
                 }
                 dataCount++;
                 progress = ((double) dataCount) / contextList.size();
+
+                count1++;
+                count2++;
+
+                // 最近500 ms的平均间隔
+                if (TimestampHelper.timestampDiff(timestamp1, getTimestamp(contextList.get(dataCount))) >= 500) {
+                    avgInterval1 = sum1 / count1;
+                    sum1 = 0;
+                    count1 = 0;
+                    timestamp1 = getTimestamp(contextList.get(dataCount));
+                }
+
+                // 最近1000 ms的平均间隔
+                if (TimestampHelper.timestampDiff(timestamp2, getTimestamp(contextList.get(dataCount))) >= 1000) {
+                    avgInterval2 = sum2 / count2;
+                    sum2 = 0;
+                    count2 = 0;
+                    timestamp2 = getTimestamp(contextList.get(dataCount));
+                }
 
                 long endTime = System.nanoTime(); //获取结束时间
                 totalTime += (endTime - startTime) ;
