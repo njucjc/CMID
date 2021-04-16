9cb18cb7a4b770f110069cd3f32f924846cd1ebf
njucjc95 <1361104459@qq.com>
2021-04-16 19:44:16 +0800

    add rules, patterns and bfunc

--- src/main/java/cn/edu/nju/util/BFuncHelper.java
+++ src/main/java/cn/edu/nju/util/BFuncHelper.java
@@ -8,11 +8,114 @@ import cn.edu.nju.context.ContextParser;
  */
 public class BFuncHelper {
 
+    private static boolean isFly(Context c) {
+        return c.getType().equals("FlyObject");
+    }
+
+    private static boolean isShip(Context c) {
+        return c.getType().equals("ShipObject");
+    }
+
+    private static boolean inFlySpeedRange(Context c) {
+        return c.getSpeed() >= 0 && c.getSpeed() <= 600;
+    }
+
+    private static boolean inShipSpeedRange(Context c) {
+        return c.getSpeed() >= 0 && c.getSpeed() <= 20;
+    }
+
+    private static boolean inLatLonRange(Context c) {
+        return c.getLongitude() >= 110 && c.getLongitude() <= 120 &&
+                c.getLatitude() >= 16 && c.getLatitude() <= 24;
+    }
+
+    private static boolean inFlyAltRange(Context c) {
+        return c.getAltitude() >= 0 && c.getAltitude() <= 20000;
+    }
+
+    private static boolean inShipAltRange(Context c) {
+        return c.getAltitude() == 0;
+    }
+
+    private static boolean inCourseRange(Context c) {
+        return c.getCourse() >= 0 && c.getCourse() <= 360;
+    }
+
+    private static boolean isSameFly(Context c1, Context c2) {
+        return c1.getNo() != c2.getNo() && isFly(c1) && isFly(c2) && c1.getId().equals(c2.getId());
+    }
+
+    private static boolean isSameShip(Context c1, Context c2) {
+        return c1.getNo() != c2.getNo() && isShip(c1) && isShip(c2) && c1.getId().equals(c2.getId());
+    }
+
+    private static boolean flyControllable(Context c1, Context c2) {
+        double sec = Math.abs(TimestampHelper.timestampDiff(c1.getTimestamp(), c2.getTimestamp())) / 1000.0;
+        double dist = LocationHelper.getDistance(c1.getLongitude(), c1.getLatitude(), c2.getLongitude(), c2.getLatitude());
+        return sec != 0 && dist / sec <= 600;
+    }
+
+    private static boolean shipControllable(Context c1, Context c2) {
+        double sec = Math.abs(TimestampHelper.timestampDiff(c1.getTimestamp(), c2.getTimestamp())) / 1000.0;
+        double dist = LocationHelper.getDistance(c1.getLongitude(), c1.getLatitude(), c2.getLongitude(), c2.getLatitude());
+        return sec != 0 && dist / sec <= 20;
+    }
+
+    private static boolean isFlyAltSuddenChange(Context c1, Context c2) {
+        return c1.getAltitude() < 6000 && c2.getAltitude() > 6000;
+    }
+
+    private static boolean inFlySpeedRangeBasedOnAlt(Context c1, Context c2) {
+        return c1.getSpeed() >= 0 && c1.getSpeed() <= 300 &&
+                c2.getSpeed() >= 0 && c2.getSpeed() <= 200;
+    }
+
 
     public static boolean bfun(String name, Context context1, Context context2) {
         boolean value = false;
         switch (name) {
-
+            case "isFly":
+                value = isFly(context1);
+                break;
+            case "isShip":
+                value = isShip(context1);
+                break;
+            case "inFlySpeedRange":
+                value = inFlySpeedRange(context1);
+                break;
+            case "inShipSpeedRange":
+                value = inShipSpeedRange(context1);
+                break;
+            case "inLatLonRange":
+                value = inLatLonRange(context1);
+                break;
+            case "inFlyAltRange":
+                value = inFlyAltRange(context1);
+                break;
+            case "inShipAltRange":
+                value = inShipAltRange(context1);
+                break;
+            case "inCourseRange":
+                value = inCourseRange(context1);
+                break;
+            case "isSameFly":
+                value = isSameFly(context1, context2);
+                break;
+            case "isSameShip":
+                value = isSameShip(context1, context2);
+                break;
+            case "flyControllable":
+                value = flyControllable(context1, context2);
+                break;
+            case "shipControllable":
+                value = shipControllable(context1, context2);
+                break;
+            case "isFlyAltSuddenChange":
+                value = isFlyAltSuddenChange(context1, context2);
+                break;
+            case "inFlySpeedRangeBasedOnAlt":
+                value = inFlySpeedRangeBasedOnAlt(context1, context2);
+                break;
             default:
                 System.out.println("[INFO] Illegal bfunc: " + name);
                 System.exit(1);
--- src/main/java/cn/edu/nju/util/LocationHelper.java
+++ src/main/java/cn/edu/nju/util/LocationHelper.java
@@ -1,5 +1,10 @@
 package cn.edu.nju.util;
 
+import jdk.nashorn.internal.runtime.GlobalConstants;
+import org.gavaghan.geodesy.Ellipsoid;
+import org.gavaghan.geodesy.GeodeticCalculator;
+import org.gavaghan.geodesy.GlobalCoordinates;
+
 /**
  * Created by njucjc on 2017/10/7.
  */
@@ -7,16 +12,13 @@ public class LocationHelper {
 
 
     public static double getDistance(double long1, double lat1, double long2, double lat2) {
-       return Math.sqrt((long1 - long2) * (long1 - long2) + (lat1 - lat2) * (lat1 - lat2));
+        GlobalCoordinates source = new GlobalCoordinates(lat1, long1);
+        GlobalCoordinates target = new GlobalCoordinates(lat2, long2);
+
+        return new GeodeticCalculator().calculateGeodeticCurve(Ellipsoid.Sphere, source, target).getEllipsoidalDistance();
     }
 
     public static void main(String[] args) {
-        //根据两点间的经纬度计算距离，单位：km
-        System.out.println(getDistance(116.5542, 39.81621, 116.5539, 39.81616));
-        System.out.println(getDistance(117.1181, 36.68484, 117.01, 36.66123));
-        System.out.println(getDistance(112.9084, 28.14203, 112.9083, 28.14194));
-        System.out.println(getDistance(121.5373, 38.86827, 121.5372, 38.86832));
-        System.out.println(getDistance(20.5, 118.2, 21.1, 117.6));
-        System.out.println(getDistance(121.445140,31.177779, 121.444832,31.179313));
+        //根据两点间的经纬度计算距离，单位：
     }
 }
